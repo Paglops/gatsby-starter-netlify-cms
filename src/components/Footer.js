116b526e3130b59f5d7b4bc5e76815a105618ff2
Sarah <34034102+Paglops@users.noreply.github.com>
2023-12-21 15:19:21 +0100

    Footer links & logo update, website logo updated, blog page title changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,8 @@ import * as React from "react";
 import { Link } from "gatsby";
 
 import logo from "../img/logo.svg";
-import facebook from "../img/social/facebook.svg";
-import instagram from "../img/social/instagram.svg";
-import twitter from "../img/social/twitter.svg";
-import vimeo from "../img/social/vimeo.svg";
+import threads from "../img/social/threads.svg";
+
 
 const Footer = class extends React.Component {
   render() {
@@ -14,7 +12,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="Mme Clette"
             style={{ width: "14em", height: "10em" }}
           />
         </div>
@@ -34,16 +32,16 @@ const Footer = class extends React.Component {
                         About
                       </Link>
                     </li>
-                    <li>
+                    {/* <li>
                       <Link className="navbar-item" to="/products">
                         Products
                       </Link>
-                    </li>
-                    <li>
+                    </li> */}
+                    {/* <li>
                       <Link className="navbar-item" to="/contact/examples">
                         Form Examples
                       </Link>
-                    </li>
+                    </li> */}
                     <li>
                       <a
                         className="navbar-item"
@@ -51,7 +49,7 @@ const Footer = class extends React.Component {
                         target="_blank"
                         rel="noopener noreferrer"
                       >
-                        Admin
+                        Log in
                       </a>
                     </li>
                   </ul>
@@ -62,7 +60,7 @@ const Footer = class extends React.Component {
                   <ul className="menu-list">
                     <li>
                       <Link className="navbar-item" to="/blog">
-                        Latest Stories
+                        Fresh from the press
                       </Link>
                     </li>
                     <li>
@@ -74,35 +72,35 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                {/* <a title="facebook" href="https://facebook.com">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: "1em", height: "1em" }}
                   />
-                </a>
-                <a title="twitter" href="https://twitter.com">
+                </a> */}
+                {/* <a title="twitter" href="https://twitter.com">
                   <img
                     className="fas fa-lg"
                     src={twitter}
                     alt="Twitter"
                     style={{ width: "1em", height: "1em" }}
                   />
-                </a>
-                <a title="instagram" href="https://instagram.com">
+                </a> */}
+                <a title="Threads" href="https://www.threads.net/@sarahclette_">
                   <img
-                    src={instagram}
-                    alt="Instagram"
+                    src={threads}
+                    alt="Threads"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
-                <a title="vimeo" href="https://vimeo.com">
+                {/* <a title="vimeo" href="https://vimeo.com">
                   <img
                     src={vimeo}
                     alt="Vimeo"
                     style={{ width: "1em", height: "1em" }}
                   />
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
