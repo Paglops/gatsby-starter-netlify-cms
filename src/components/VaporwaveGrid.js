ceeb6b3fd8922caf008e5eed81492a60c7a9ae1b
Sarah <34034102+Paglops@users.noreply.github.com>
2023-12-22 02:50:25 +0100

    Vaporwave grid - Implem & test 0

---
+++ src/components/VaporwaveGrid.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import "VaporwaveGrid.sass";
+
+const VaporwaveGrid = () => {
+    <div>
+        <section class="content">
+            <p class="top">Mme Clette</p>
+            <h1>HACKERPERSON</h1>
+            <p class="bottom">coming in 2024</p>
+        </section>
+        <section class="lighth">
+        <section class="lightv"></section>
+        </section>
+    </div>
+}
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -3,52 +3,57 @@ import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
+import VaporwaveGrid from "../components/VaporwaveGrid"
 
 // eslint-disable-next-line
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+    <VaporwaveGrid></VaporwaveGrid>
+  )
 
-AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
-};
+//   return (
+//     <section className="section section--gradient">
+//       <div className="container">
+//         <div className="columns">
+//           <div className="column is-10 is-offset-1">
+//             <div className="section">
+//               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+//                 {title}
+//               </h2>
+//               <PageContent className="content" content={content} />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
 
-const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data;
+// AboutPageTemplate.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   content: PropTypes.string,
+//   contentComponent: PropTypes.func,
+// };
 
-  return (
-    <Layout>
-      <AboutPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
-      />
-    </Layout>
-  );
-};
+// const AboutPage = ({ data }) => {
+//   const { markdownRemark: post } = data;
 
-AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
-};
+//   return (
+//     <Layout>
+//       <AboutPageTemplate
+//         contentComponent={HTMLContent}
+//         title={post.frontmatter.title}
+//         content={post.html}
+//       />
+//     </Layout>
+//   );
+// };
+
+// AboutPage.propTypes = {
+//   data: PropTypes.object.isRequired,
+// };
 
 export default AboutPage;
 
